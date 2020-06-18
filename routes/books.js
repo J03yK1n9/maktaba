7769ec4c3e814a2d1a0a650d714e4537e6e04882
J03yK1n9 <joeykingbtc@gmail.com>
2020-06-18 22:37:54 +0300

    book routes

---
+++ routes/books.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const Book = require("../models/book");
+const Author = require("../models/author");
+const path = require("path");
+const uploadPath = path.join('public', Book.coverImageBasePath)
+const multer = require("multer");
+const imageMimeTypes = ['image/jpeg','image/png','image/gif']
+const upload = multer({
+  dest: uploadPath,
+  fileFilter: (req, file, callback) => {
+    callback(null, imageMimeTypes.includes(file.mimetype))
+  }
+})
+
+// All Books Route
+router.get("/", async (req, res) => {
+    res.send('All Books')
+});
+
+// New Books Route
+router.get("/new", async (req, res) => {
+    renderNewPage(res, new Book())
+});
+
+// Create Books Route
+router.post("/", upload.single('cover'), async (req, res) => {
+  const fileName = req.file != null ? req.file.filename : null
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    publishDate: new Date(req.body.publishDate),
+    pageCount: req.body.pageCount,
+    coverImageName: fileName,
+    description: req.body.description
+  })
+
+  try {
+    const newBook = await book.save()
+    // res.redirect(`books/${newBook.id}`)
+    res.redirect(`books`);
+  } catch {
+    renderNewPage(res, book, true)
+  }
+});
+
+// render books function
+async function renderNewPage(res, book, hasError = false) {
+  try {
+    const authors = await Author.find({})
+    const params = {
+      authors: authors,
+      book: book
+    }
+    if (hasError) params.errorMessage = 'Error Creating Book, please try again and confirm you have the right parameters'
+    res.render('books/new', params)
+  } catch {
+    res.redirect('/books')
+  }
+}
+
+module.exports = router;
